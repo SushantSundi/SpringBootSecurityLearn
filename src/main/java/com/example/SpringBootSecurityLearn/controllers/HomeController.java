56c3e84b0a0e2f53add5b97bcf3175a070d6f0bd
SurajSushant <surajsushant007@gmail.com>
2024-06-21 07:46:24 +0530

    #added security config

--- src/main/java/com/example/SpringBootSecurityLearn/controllers/HomeController.java
+++ src/main/java/com/example/SpringBootSecurityLearn/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.SpringBootSecurityLearn.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class HomeController {
 
+//    @PreAuthorize("hasRole('ROLE_NORMAL')")
     @GetMapping("/normal")
     public String getNormal() {
         return "Hi you hit Normal Api";
     }
 
+//    @PreAuthorize("hasRole('ROLE_NORMAL')")
     @GetMapping("/admin")
     public String getAdmin() {
         return "Hi you hit Admin Api";
